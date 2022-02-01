2f4b28a79b301d5de0b26d4e72aebf4423fca028
Marc Cavazos <marcavazos19@icloud.com>
2022-02-01 10:57:06 -0500

    commit tree 362a2b491a0b9dbf5e2f49e7d237db0ef933c43d

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,11 @@
 // Code Keypad Component Here
+function Keypad() {
+    function handleClick() {
+        console.log('Entering password...')
+    }
+    return (
+        <input type="password" onChange={handleClick}/>
+    )
+}
+
+export default Keypad;
